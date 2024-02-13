e4f42c40879350e86f8d0c88e4b2c24ee62b0c4f
sulumor <romuald.patfoort@gmail.com>
2024-02-13 16:19:30 +0100

    feat: add CORS policy

--- app/index.app.js
+++ app/index.app.js
@@ -1,12 +1,19 @@
 import express from "express";
+import cors from "cors";
 import router from "./routers/index.router.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import createDoc from "./helpers/swagger.doc.js";
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+};
 
 const app = express();
 
 createDoc(app);
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(router);
 app.use(errorMiddleware);
