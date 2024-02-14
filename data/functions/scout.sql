9d7a3bf7095354ab4bd50a90f7e5a0949c581469
sulumor <romuald.patfoort@gmail.com>
2024-02-14 16:51:43 +0100

    refacto: update structure date folder

---
+++ data/functions/scout.sql
@@ -0,0 +1,22 @@
+-- SQLBook: Code
+BEGIN;
+
+CREATE FUNCTION "update_scout"(json) RETURNS "scout_view" AS $$
+    UPDATE "user" SET
+    "firstname" = COALESCE($1->>'firstname', "firstname"),
+    "lastname" = COALESCE($1->>'lastname', "lastname"),
+    "email" = COALESCE($1->>'email', "email"),
+    "password" = COALESCE($1->>'password', "password")
+    WHERE id = ($1->>'id')::int;
+
+    UPDATE "scout" SET
+    "club" = COALESCE($1->>'club', "club"),
+    "city" = COALESCE($1->>'city', "city"),
+    "updated_at" = now()
+    WHERE "user_id" = ($1->>'id')::int;
+
+    SELECT * FROM "scout_view" WHERE "id" = ($1->>'id')::int;
+
+$$ LANGUAGE sql STRICT;
+
+COMMIT;
--- data/seeding/structure.sql
+++ data/seeding/structure.sql
@@ -1,7 +1,10 @@
 
 BEGIN;
 
-
+DROP FUNCTION IF EXISTS "update_player";
+DROP FUNCTION IF EXISTS "update_scout";
+DROP VIEW IF EXISTS "player_view";
+DROP VIEW IF EXISTS "scout_view";
 DROP TABLE IF EXISTS "role","user", "position", "player", "scout", "team","meet", "play", "match", "link", "statistics", "follow";
 
 
