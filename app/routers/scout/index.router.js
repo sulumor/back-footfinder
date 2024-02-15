06bfa4be543502169c240a0450e5323218c26684
sulumor <romuald.patfoort@gmail.com>
2024-02-15 08:30:26 +0100

    feat: MEP view player in player datamapper

--- app/routers/scout/index.router.js
+++ app/routers/scout/index.router.js
@@ -5,17 +5,16 @@ import ScoutController from "../../controllers/scout.controller.js";
 const scoutRouter = Router();
 
 scoutRouter.route("/:id")
-.get(controllerWrapper(ScoutController.getWithUser.bind(ScoutController)))
-.patch(controllerWrapper(ScoutController.updateInfos.bind(ScoutController)));
+  .get(controllerWrapper(ScoutController.getWithUser.bind(ScoutController)))
+  .patch(controllerWrapper(ScoutController.updateInfos.bind(ScoutController)));
 
 scoutRouter.route("/search")
-.get(controllerWrapper(ScoutController.getAll.bind(ScoutController)));
+  .get(controllerWrapper(ScoutController.getAll.bind(ScoutController)));
 
 scoutRouter.route("/:id/player:/id")
-.get(controllerWrapper(ScoutController.getByPk.bind(ScoutController)));
+  .get(controllerWrapper(ScoutController.getByPk.bind(ScoutController)));
 
 scoutRouter.route("/:id/player/:id/match/:id")
-.get(controllerWrapper(ScoutController.getByPk.bind(ScoutController)));
-
+  .get(controllerWrapper(ScoutController.getByPk.bind(ScoutController)));
 
 export default scoutRouter;
