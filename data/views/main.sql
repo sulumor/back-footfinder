204f53a6b971e8833598145ec80fa9e4c14ca278
sulumor <romuald.patfoort@gmail.com>
2024-02-20 10:23:54 +0100

    fix: update fakers with new db

--- data/views/main.sql
+++ data/views/main.sql
@@ -1,10 +1,13 @@
 BEGIN;
 
+DROP VIEW IF EXISTS "player_view","scout_view","statistics_view","team_view","match_view";
+
 CREATE VIEW match_view AS
   SELECT
     "player"."user_id" AS "id",
     "match"."id" AS "match_id",
     "score",
+    "date",
     "team_id_as_home",
     "team_id_as_outside"
   FROM "match"
@@ -22,6 +25,8 @@ CREATE VIEW player_view AS
         "nationality", 
         "avatar",
         "genre",
+        "height",
+        "weight",
         "strong_foot",
         ARRAY_AGG( DISTINCT "link"."team_id") AS "team_id",
         ARRAY_AGG( DISTINCT "follow"."scout_id") AS "scout_id", 
@@ -33,7 +38,7 @@ CREATE VIEW player_view AS
         JOIN "follow" ON "player"."id" = "follow"."player_id" 
         JOIN "link" ON "link"."player_id" = "player"."id"
         JOIN "team" ON "link"."team_id" = "team"."id"
-    GROUP BY "user"."id","player"."id", "player"."birth_date", "player"."nationality", "player"."genre", "player"."strong_foot","position"."label", "player"."number_of_matches_played";
+    GROUP BY "user"."id","player"."id", "player"."birth_date", "player"."nationality", "player"."genre", "player"."strong_foot","position"."label", "player"."number_of_matches_played","player"."height","player"."weight";
 
 CREATE VIEW scout_view AS
     SELECT
@@ -56,6 +61,7 @@ CREATE VIEW statistics_view AS
         "player"."id" AS "id",
         "match"."id" AS "match_id",
         "score",
+        "date",
         "team_id_as_home",
         "team_id_as_outside",
         "assists",
