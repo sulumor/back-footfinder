e18d92751e0bdde5713f277fab7fb73f2a78a3cf
sulumor <romuald.patfoort@gmail.com>
2024-02-27 12:22:35 +0100

    mise en prod

--- app/helpers/pg.client.js
+++ app/helpers/pg.client.js
@@ -4,6 +4,7 @@ const client = new pg.Pool({
   host: process.env.PGHOST,
   user: process.env.PGUSER,
   database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
 });
 
 export default client;
