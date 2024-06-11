36c205b18d88bed3d5ce7a95cd5b1dd15125ab6d
sulumor <romuald.patfoort@gmail.com>
2024-06-11 17:34:04 +0200

    fix

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -39,7 +39,6 @@ export default class AuthController extends CoreController {
       httpOnly: true,
       secure: true,
       path: "/",
-      domain: process.env.DOMAIN,
       sameSite: "none",
     });
 
