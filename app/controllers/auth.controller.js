741d6dd48f17a7cf24c43b0b8aa7227bec3af5b1
sulumor <romuald.patfoort@gmail.com>
2024-06-11 16:01:58 +0200

    fix: third part cookies

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -35,7 +35,11 @@ export default class AuthController extends CoreController {
       : await ScoutDatamapper.findAll({ where: { email: body.email } });
     if (!data) return next(new ApiError(errorMessage, errorInfos));
 
-    res.cookie("refresh_token", createRefreshToken(data), { httpOnly: true, secure: true, sameSite: "none" });
+    res.cookie("refresh_token", createRefreshToken(data), {
+      httpOnly: true,
+      secure: true,
+      path: "/refresh_token",
+    });
 
     return res.status(200).json({ accessToken: createAccessToken(data) });
   }
